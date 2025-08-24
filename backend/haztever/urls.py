96a9fd63867d258a2b787ef72bf1c0da8287b88f
Jhan Carvajal <jan.carvajal1@u.icesi.edu.co>
2025-08-23 19:06:08 -0500

    FINALLY!!! The API is deployed using lambda and aurora

--- backend/haztever/urls.py
+++ backend/haztever/urls.py
@@ -1,23 +1,21 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
+from django.urls import include
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('users/', include('haztever.users.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     
+    path('admin/', admin.site.urls),
+   
+    path('users/', include('haztever.users.urls')),
+    path('accounts/', include('allauth.urls')), 
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/business/', include('haztever.business.urls')),
     path('api/catalog/', include('haztever.catalog.urls')),
-    
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
